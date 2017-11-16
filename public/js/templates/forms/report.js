83ae710db18da71d8e0c23c8bc6ba0c0a5d2c59d
Creighton Long <clong@zendesk.com>
2017-11-15 23:12:42 -0600

    Implement report form

--- public/js/templates/forms/report.js
+++ public/js/templates/forms/report.js
@@ -1,7 +1,43 @@
 var reportFormTemplate = $.templates('reportForm',
 
-"<div class='report-form'>" +
-"  This is where the report form goes." +
+"<div class='report-form' id='{{:domID}}'>" +
+"  <div class='field halfsies'>" +
+"    <label>" +
+"      Contact name" +
+"    </label>" +
+"    <div class='field-input'>" +
+"      <input name='submitter-name'></input>" +
+"    </div>" +
+"  </div>" +
+"  <div class='field halfsies'>" +
+"    <label>" +
+"      Tax year" +
+"    </label>" +
+"    <div class='field-input'>" +
+"      <input name='tax-year'></input>" +
+"    </div>" +
+"  </div>" +
+"  <div class='field halfsies'>" +
+"    <label>" +
+"      Contact phone number and extension" +
+"    </label>" +
+"    <div class='field-input'>" +
+"      <div class='field-subinput phone'>" +
+"        <input name='submitter-phone'></input>" +
+"      </div>" +
+"      <div class='field-subinput phone-ext'>" +
+"        <input name='submitter-phone-ext'></input>" +
+"      </div>" +
+"    </div>" +
+"  </div>" +
+"  <div class='field halfsies'>" +
+"    <label>" +
+"      Contact email" +
+"    </label>" +
+"    <div class='field-input'>" +
+"      <input name='submitter-email'></input>" +
+"    </div>" +
+"  </div>" +
 "</div>"
 
 );
