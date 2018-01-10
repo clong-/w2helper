1f32fe69e441f4ca5f11e134f730f2e0de1866d1
Creighton Long <clong@zendesk.com>
2018-01-09 18:05:04 -0600

    Move employee controls

--- public/js/templates/employee.js
+++ public/js/templates/employee.js
@@ -1,11 +1,13 @@
 var employeeTemplate = $.templates('employee',
 
 "<div id='{{:domID}}' class='employee box'>" +
-"  <h1>Employee Name Here!</h1>" +
-"  <div class='employee-forms'>" +
+"  <div class='employee-title title'>" +
+"    <h1>Employee Name Here!</h1>" +
 "  </div>" +
 "  <div class='employee-controls controls'>" +
 "    <button id='remove-{{:domID}}' class='btn btn-default remove-employee'>Remove Employee</button>" +
+"  </div>" +
+"  <div class='employee-forms'>" +
 "  </div>" +
 "</div>"
 
