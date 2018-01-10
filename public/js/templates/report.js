658c51c2ff484bff59c41e06ec5242b23252d247
Creighton Long <clong@zendesk.com>
2018-01-09 18:36:37 -0600

    Move report controls

--- public/js/templates/report.js
+++ public/js/templates/report.js
@@ -1,12 +1,14 @@
 var reportTemplate = $.templates('report',
 
 "<div id='{{:domID}}' class='box'>" +
-"  <h2>Submitter Information</h2>" +
-"  <div class='report-forms'>" +
+"  <div class='report-title title'>" +
+"    <h2>Submitter Information</h2>" +
 "  </div>" +
 "  <div class='report-controls controls'>" +
 "    <button id='show-file-modal' class='btn btn-default'>Show File Modal</button>" +
 //"    <button id='add-business-to-{{:domID}}' class='btn btn-default'>Add Business</button>" + //multiple businesses
+"  </div>" +
+"  <div class='report-forms'>" +
 "  </div>" +
 "  <div class='businesses-pane viewport'>" +
 "  </div>" +
